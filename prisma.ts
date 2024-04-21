07e767bc8fa410fc07ddbb8fc797267bfc0ae12a
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2024-04-21 01:15:52 +0100

    feat:Cretaed file and folder structure

---
+++ prisma.ts
@@ -0,0 +1,4 @@
+import { PrismaClient } from "@prisma/client";
+
+/* Config database */
+const prisma = new PrismaClient();
---
+++ prisma/migrations/20240420232131_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Type" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "PropertyListing" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
